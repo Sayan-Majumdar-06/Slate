2b28bebcdd4bf62d0b7c5534d313249f95420cd2
Sayan-Majumdar-06 <sayanmajumdar.edu.3101@gmail.com>
2026-07-10 17:10:15 +0530

    style: fixed UI for home page

--- Client/src/routes/Home.tsx
+++ Client/src/routes/Home.tsx
@@ -17,13 +17,45 @@ export default function Home() {
     navigate(`/create/${roomID}`, { state: {username: username} });
   }
   return (
-    <div className="w-screen h-screen bg-zinc-900 flex flex-col">
-      <header className="pt-14 pl-16 flex flex-col gap-2">
-        <h1 className="text-zinc-200 geomini text-5xl font-semibold">Slate</h1>
-        
-      </header>
+    <div className="w-screen h-screen bg-zinc-900 flex">
+      <section className="w-[52%] flex flex-col justify-center items-end gap-2 p-20 pr-12">
+        <div className="text-left flex flex-col gap-2">
+          <div className="flex gap-2 items-center">
+            <div className="group h-fit text-emerald-500 font-bold text-md border-2 border-zinc-700 bg-zinc-800 rounded-xl flex items-center justify-center p-2 geomini">
+              <span className="group-hover:rotate-y-360 transition-transform duration-500">{"</>"}</span>
+            </div>
 
-      <section className="w-full grow flex justify-center items-center">
+            <h1 className="text-zinc-200 geomini text-5xl font-semibold">Slate</h1>
+          </div>
+          
+          
+          <h3 className="text-zinc-400 font-medium text-2xl max-w-sm">
+            Conduct technical interviews in one shared workspace.
+          </h3>
+
+          <ul className="mt-8 list-disc pl-6 text-zinc-400 flex flex-col gap-2 max-w-lg">
+            <li>
+              <h1 className="font-medium text-lg">Real-time collaborative coding</h1>
+            </li>
+            <li>
+              <h1 className="font-medium text-lg">Shared whiteboard</h1>
+            </li>
+            <li>
+              <h1 className="font-medium text-lg">Auto-save session progress</h1>
+            </li>
+            <li>
+              <h1 className="font-medium text-lg">Interviewer controls</h1>
+            </li>
+            <li>
+              <h1 className="font-medium text-lg">Low-latency synchronization</h1>
+            </li>
+          </ul>
+
+          <p className="mt-4 font-medium text-lg text-zinc-400 max-w-lg">No Signup required.</p>
+        </div>
+      </section>
+
+      <section className="grow flex items-center">
         <div className="w-[400px] py-8 px-12 border rounded-xl flex flex-col gap-4 justify-center bg-zinc-800 border-zinc-600 figtree">
 
           <h2 className="text-zinc-300 text-xl mb-2">Join a room</h2>
